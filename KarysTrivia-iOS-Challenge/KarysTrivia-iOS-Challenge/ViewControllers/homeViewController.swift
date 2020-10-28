a25688c24c2cd193758a6c978046f93608427f30
karyRmartinez <karymartinez@pursuit.org>
2020-10-28 13:35:10 -0400

    title label UI and Constraints

--- KarysTrivia-iOS-Challenge/KarysTrivia-iOS-Challenge/ViewControllers/homeViewController.swift
+++ KarysTrivia-iOS-Challenge/KarysTrivia-iOS-Challenge/ViewControllers/homeViewController.swift
@@ -19,23 +19,38 @@ class homeViewController: UIViewController {
         button.layer.cornerRadius = 12
         return button
     }()
-    
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .white
+        label.textColor = .systemBlue
+        label.textAlignment = .center
+        label.text = "Tandem Trvia"
+        label.font = UIFont(name: "savoyeLetPlain", size: 50)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         addSubviews()
         setButtonConstraints()
     }
     
     private func addSubviews() {
         self.view.addSubview(startButton)
+        self.view.addSubview(titleLabel)
     }
     
     private func setButtonConstraints() {
-        NSLayoutConstraint.activate([
-            startButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            startButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 150),
-            startButton.widthAnchor.constraint(equalToConstant: 130),
-            startButton.heightAnchor.constraint(equalToConstant: 35)
+    NSLayoutConstraint.activate([
+    startButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+    startButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 150),
+    startButton.widthAnchor.constraint(equalToConstant: 130),
+    startButton.heightAnchor.constraint(equalToConstant: 35),
+            
+    titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 120),
+    titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                  
         ])
     }
  
